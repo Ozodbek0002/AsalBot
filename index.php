4e287d6aaf556f9e3dd95b5024fa781dd50d1b39
Ozodbek <ozodbekozodov0002@gmail.com>
2022-08-15 15:59:49 +0500

    .

--- index.php
+++ index.php
@@ -7,4 +7,6 @@ $telegram = new Telegram('5516988832:AAGFKzAY-Y2Q8arkPi18PSC2zGfwMPS0JnI');
 $chat_id = $telegram->ChatID();  // foydalanuvchi ID si
 $chat_name = $telegram->FirstName();  // foydalanuvchi nomi
 $text = $telegram->Text(); // foydalanuvchi yuborgan text
-$starText = " Assalomu Alaykum bu botimiz shunchaki tajriba asosina ishlayapti yana notog'ri hayollarga borib yurmang birodar 😉 ";
+
+$content = array('chat_id' => $chat_id, 'text' => $text );
+$telegram->sendMessage($content);
