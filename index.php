6329bff0c980b62ebac0d1fa2a3488e1f6658f25
Ozodbek <ozodbekozodov0002@gmail.com>
2022-08-15 16:01:46 +0500

    .

--- index.php
+++ index.php
@@ -8,5 +8,10 @@ $chat_id = $telegram->ChatID();  // foydalanuvchi ID si
 $chat_name = $telegram->FirstName();  // foydalanuvchi nomi
 $text = $telegram->Text(); // foydalanuvchi yuborgan text
 
+if ($text == 'salom'){
+    $content = array('chat_id' => $chat_id, 'text' => 'Va Alaykum Assalom' );
+    $telegram->sendMessage($content);
+}
+
 $content = array('chat_id' => $chat_id, 'text' => $text );
 $telegram->sendMessage($content);
