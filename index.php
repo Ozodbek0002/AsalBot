2f3e7990cdd86d38bf762053bd2c114dd306c9bc
Ozodbek <ozodbekozodov0002@gmail.com>
2022-08-17 01:27:50 +0500

    .

--- index.php
+++ index.php
@@ -8,10 +8,54 @@ $chat_id = $telegram->ChatID();  // foydalanuvchi ID si
 $chat_name = $telegram->FirstName();  // foydalanuvchi nomi
 $text = $telegram->Text(); // foydalanuvchi yuborgan text
 
-if ($text == 'salom'){
-    $content = array('chat_id' => $chat_id, 'text' => 'Va Alaykum Assalom' );
+if ($text == '/start') {
+    showStart();
+} elseif ($text == '🍯 Biz haqimizda ') {
+    $content = [
+        'chat_id' => $chat_id,
+        'text' => " Biz haqimizda bilib oling <a href='https://telegra.ph/Biz-haqimizda-08-10'>Link</a> ",
+        'parse_mode' => "html"
+    ];
     $telegram->sendMessage($content);
+} elseif ($text == '🍯 Buyurtma berish') {
+
+
 }
 
-$content = array('chat_id' => $chat_id, 'text' => $text );
-$telegram->sendMessage($content);
+
+//Funksiyalar
+function showStart()
+{
+    global $telegram, $chat_id, $starText;
+    $option = [
+        [
+            $telegram->buildKeyboardButton("🍯 Biz haqimizda")
+        ],
+        [
+            $telegram->buildKeyboardButton("🍯 Buyurtma berish")
+        ]
+    ];
+
+    $keyb = $telegram->buildKeyBoard($option, $onetime = false, $resize = true);
+    $content = [
+        'chat_id' => $chat_id,
+        'reply_markup' => $keyb,
+        'text' => " Assalomu Alaykum biz sof va tabiy asal bilan shug'ullanamiz  ",
+    ];
+
+    $telegram->sendMessage($content);
+
+}
+
+function showContact()
+{
+    global $telegram, $chat_id, $starText;
+
+
+}
+
+function getNumber()
+{
+
+}
+
