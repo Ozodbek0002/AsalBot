1c7397617bd074ee09bfaf852f775f40b0e6550d
Ozodbek <ozodbekozodov0002@gmail.com>
2022-08-18 12:31:06 +0500

    .

--- index.php
+++ index.php
@@ -8,16 +8,28 @@ $chat_id = $telegram->ChatID();  // foydalanuvchi ID si
 $chat_name = $telegram->FirstName();  // foydalanuvchi nomi
 $text = $telegram->Text(); // foydalanuvchi yuborgan text
 
-file_put_contents('users/step.txt','1');
+file_put_contents('users/step.txt', '1');
 $stepfile = file_get_contents('users/step.txt');
-echo $stepfile;
 
-$orderTypes = ["1kg = 25 000 sum","2kg = 50 000 sum","3kg = 75 000 sum","4kg = 100 000 sum" ];
+$orderTypes = ["1kg = 25 000 sum", "2kg = 50 000 sum", "3kg = 75 000 sum", "4kg = 100 000 sum"];
 
 if ($text == '/start') {
     showStart();
+} elseif ($text == '🍯 Biz haqimizda') {
+    showAbout();
+} elseif ($text == '🍯 Buyurtma berish') {
+    showOrder();
+} elseif (in_array($text, $orderTypes)) {
+    askContact();
+} else {
+    askContact();
 }
-elseif ($text == '🍯 Biz haqimizda') {
+
+
+//Funksiyalar
+function showAbout()
+{
+    global $chat_id, $telegram;
     $content = [
         'chat_id' => $chat_id,
         'text' => " Biz haqimizda bilib oling <a href='https://telegra.ph/Biz-haqimizda-08-10'>Link</a> ",
@@ -25,21 +37,10 @@ elseif ($text == '🍯 Biz haqimizda') {
     ];
     $telegram->sendMessage($content);
 }
-elseif ($text == '🍯 Buyurtma berish') {
-    showOrder();
-}
-elseif (in_array($text,$orderTypes)) {
-    askContact();
-}
-else{
-    askContact();
-}
 
-
-//Funksiyalar
 function showStart()
 {
-    global $telegram, $chat_id ;
+    global $telegram, $chat_id;
     $option = [
         [
             $telegram->buildKeyboardButton("🍯 Biz haqimizda")
@@ -62,18 +63,18 @@ function showStart()
 
 function showOrder()
 {
-    global $telegram, $chat_id ;
+    global $telegram, $chat_id;
     $option = [
         [
             $telegram->buildKeyboardButton("1kg = 25 000 sum")
         ],
-         [
+        [
             $telegram->buildKeyboardButton("2kg = 50 000 sum")
         ],
-         [
+        [
             $telegram->buildKeyboardButton("3kg = 75 000 sum")
         ],
-         [
+        [
             $telegram->buildKeyboardButton("4kg = 100 000 sum")
         ],
     ];
@@ -84,19 +85,18 @@ function showOrder()
         'reply_markup' => $keyb,
         'text' => " Hajim tanlandi endi telefon raqamingizni jo`natsangiz ",
     ];
-
     $telegram->sendMessage($content);
 
 }
 
 function askContact()
 {
-    global $telegram, $chat_id ;
+    global $telegram, $chat_id;
+    file_put_contents('/users/step.txt', 'phone');
     $option = [
         [
-            $telegram->buildKeyboardButton("Raqamni jo`natish")
+            $telegram->buildKeyboardButton("Raqamni jo`natish",$request_contact = true)
         ],
-
     ];
 
     $keyb = $telegram->buildKeyBoard($option, $onetime = false, $resize = true);
@@ -105,14 +105,6 @@ function askContact()
         'reply_markup' => $keyb,
         'text' => " Malumot qabul qilindi ",
     ];
-
     $telegram->sendMessage($content);
-
-}
-
-
-function getNumber()
-{
-
 }
 
